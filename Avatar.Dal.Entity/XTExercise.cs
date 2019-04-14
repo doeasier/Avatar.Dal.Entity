9515af06a17f7da45ccbedf41d08d96b4630438e
scott156 <jiage.su@gmail.com>
2019-04-14 22:06:31 +0800

    增加XTExercise表

---
+++ Avatar.Dal.Entity/XTExercise.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using XFramework.Dal.Orm;
+
+namespace Avatar.Dal.Entity
+{
+    [Table(Name = "xt_exercises")]
+    [Database(Name = "EZLearnDB")]
+    public class XTExercise
+    {
+        /// <summary>
+        /// Id, 主键
+        /// </summary>
+        [Column(Name = "id", PrimaryKey = true, ColumnType = DbType.AnsiString)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 员工编号
+        /// </summary>
+        [Column(Name = "employee_id", ColumnType = DbType.AnsiString)]
+        public string EmployeeId { get; set; }
+
+        /// <summary>
+        /// 员工编号
+        /// </summary>
+        [Column(Name = "exercises_parent", ColumnType = DbType.AnsiString)]
+        public string ExerciseParent { get; set; }
+
+        /// <summary>
+        /// 是否是变式题
+        /// </summary>
+        [Column(Name = "is_change", ColumnType = DbType.Int32)]
+        public int? IsChange { get; set; }
+    }
+}
