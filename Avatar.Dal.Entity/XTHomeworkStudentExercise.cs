484916fa1026a55b7c6c0af4c61e0e331e3f4c4b
scott156 <jiage.su@gmail.com>
2019-04-14 16:46:56 +0800

    增加XTHomeworkStudentExercise

---
+++ Avatar.Dal.Entity/XTHomeworkStudentExercise.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using XFramework.Dal.Orm;
+
+namespace Avatar.Dal.Entity
+{
+    [Table(Name = "xt_homework_student_exercises")]
+    [Database(Name = "EZLearnDB")]
+    public class XTHomeworkStudentExercise
+    {
+        /// <summary>
+        /// Id, 主键
+        /// </summary>
+        [Column(Name = "id", PrimaryKey = true, ColumnType = DbType.AnsiString)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 习题Id
+        /// </summary>
+        [Column(Name = "xt_exercises_id", ColumnType = DbType.AnsiString)]
+        public string ExercisesId { get; set; }
+
+        /// <summary>
+        /// 答案类型（1-优秀，2-典型）
+        /// </summary>
+        [Column(Name = "answer_flag", ColumnType = DbType.Int32)]
+        public int? AnswerFlag { get; set; }
+
+        /// <summary>
+        /// 答案的图片地址
+        /// </summary>
+        [Column(Name = "answer_img", ColumnType = DbType.AnsiString)]
+        public string AnswerImage { get; set; }
+    }
+}
