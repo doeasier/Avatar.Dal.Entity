6a1175ca6d870ea592d5521771ed3c1635924336
jiasu <jiasu@Ctrip.com>
2019-06-17 15:24:15 +0800

    增加新表

---
+++ Avatar.Dal.Entity/XTHomeworkExercisesKnowhow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using XFramework.Dal.Orm;
+
+namespace Avatar.Dal.Entity
+{
+    // 批阅明细
+    [Table(Name = "xt_homework_exercises_knowhow")]
+    [Database(Name = "EZLearnDB")]
+    public class XTHomeworkExerciseKnowhow
+    {
+        /// <summary>
+        /// Id, 主键
+        /// </summary>
+        [Column(Name = "id", PrimaryKey = true, ColumnType = DbType.AnsiString)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 作业Id
+        /// </summary>
+        [Column(Name = "xt_homework_id", ColumnType = DbType.AnsiString)]
+        public string HomeworkId { get; set; }
+    }
+}
